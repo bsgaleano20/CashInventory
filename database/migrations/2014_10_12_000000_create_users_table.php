d8bc64c3764b47d3246753ca601fa132dc84f44c
bsgaleano20 <94256618+bsgaleano20@users.noreply.github.com>
2023-03-14 00:46:18 -0500

    Laravel Bleeze

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('Id_user');
             $table->string('name');
+            $table->string('phone');
+            $table->string('adress');
+            $table->string('enable');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('Rol_id_rol');
         });
     }
 
